05e30f748a669103ea338a4f4eed0191a45ca5eb
Yonathan Hilkia <yonathanhilkia54@gmail.com>
2026-04-21 18:37:33 +0700

    deleted unnecessary files

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from "next/navigation";
+import { redirect } from "next/navigation"
 
-export default function HomePage() {
-  redirect("/excel-parse");
+export default function Page() {
+  redirect("/components/excel-parse")
 }
